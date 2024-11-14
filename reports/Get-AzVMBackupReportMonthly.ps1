11afbf5c17ca677ce08db5008e8a833f74fddc94
kreatd <115745985+kreatd@users.noreply.github.com>
2024-11-14 13:11:47 -0500

    Update Get-AzVMBackupReportMonthly.ps1

--- reports/Get-AzVMBackupReportMonthly.ps1
+++ reports/Get-AzVMBackupReportMonthly.ps1
@@ -1,35 +1,39 @@
 
-connect-azaccount -identity
+connect-azaccount -identity -Environment AzureUSGovernment
 
+#variables
 $backup_Output = New-Object System.Collections.ArrayList
-
 $vaults = Get-AzRecoveryServicesVault
 
-foreach($vault in $vaults){
-$backup_Items = @()
-Set-AzRecoveryServicesVaultContext -Vault $vault
+#loop through each vault and obtain every item within every RSV backup container and append the values to an array.
+foreach($vault in $vaults)
+{
+    $backup_Items = @()
+    Set-AzRecoveryServicesVaultContext -Vault $vault
 
-$backup_Containers = Get-AzRecoveryServicesBackupContainer -ContainerType AzureVM
+    $backup_Containers = Get-AzRecoveryServicesBackupContainer -ContainerType AzureVM
 
-foreach($backup_Container in $backup_Containers){
-$backup_Items += Get-AzRecoveryServicesBackupItem -Container $backup_Container -WorkloadType AzureVM
+    foreach($backup_Container in $backup_Containers)
+    {
+        $backup_Items += Get-AzRecoveryServicesBackupItem -Container $backup_Container -WorkloadType AzureVM
+    }
+
+    #loop through every backup item and add the necessary values to the backup output object
+    foreach($backup_Item in $backup_Items)
+    {
+        $backup_Output.Add([PSCustomObject]@{
+        'VM Name' = $backup_Item.Name.split(";")[-1]
+        'Resource Group Name' = $backup_Item.Name.split(";")[-2]
+        'Last Backup Status' = $backup_Item.LastBackupStatus
+        'Protection Status' = $backup_Item.ProtectionStatus
+        'Last Backup Time' = $backup_Item.LastBackupTime
+        'Backup Policy Name' = $backup_Item.ProtectionPolicyName
+        'Recovery Services Vault' = $backup_item.policyid.split("/")[-3]
+        })
+    }
 }
 
-
-foreach($backup_Item in $backup_Items){
-
-  $backup_Output.Add([PSCustomObject]@{
-  'VM Name' = $backup_Item.Name.split(";")[-1]
-  'Resource Group Name' = $backup_Item.Name.split(";")[-2]
-  'Last Backup Status' = $backup_Item.LastBackupStatus
-  'Protection Status' = $backup_Item.ProtectionStatus
-  'Last Backup Time' = $backup_Item.LastBackupTime
-  'Backup Policy Name' = $backup_Item.ProtectionPolicyName
-  'Recovery Services Vault' = $backup_item.policyid.split("/")[-3]
-})
-}
-}
-
+#convert the backup output to html for email formatting
 $emailOutput = $backup_output | convertTo-Html
 
  $Style = @"
@@ -48,7 +52,7 @@ $emailOutput = $backup_output | convertTo-Html
     th {
         font-size: 1.2em;
         text-align: left;
-        background-color: #771b61;
+        background-color: darkblue;
         color: #ffffff;
         }
     td {
@@ -64,7 +68,7 @@ $emailOutput = $backup_output | convertTo-Html
     $Style
     <html>
     <body>
-        $emailOutput
+    $emailOutput
     </body>
     </html>
 "@
@@ -77,7 +81,7 @@ $emailParams = @{
 "token" = $token
 "emailRecipient" = ""
 "fromAddress" = ""
-"msgSubject" = "Azure VM Backup Report"
+"msgSubject" = "Azure VM Monthly Backup Report"
 "htmlbody" = $emailBody
 }
 
