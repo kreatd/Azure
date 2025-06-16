af5ec9cec104e67cee9afdf2811311ed11cfd155
kreatd <115745985+kreatd@users.noreply.github.com>
2025-06-16 11:54:33 -0400

    Create pullvmreport.ps1

---
+++ reports/pullvmreport.ps1
@@ -0,0 +1,28 @@
+$vms = get-azvm -status | where {$_.name.Length -le 16}
+$vmoutput = New-Object System.Collections.ArrayList
+foreach ($vm in $vms) {
+    $nicid = ($vm.NetworkProfile.NetworkInterfaces)[0].Id
+    $nic = Get-AzResource -ResourceId $nicid
+    $compute = get-azvmsize -vmname $vm.name -resourcegroupname $vm.resourcegroupname | where {$_.name -eq $vm.hardwareprofile.vmsize}
+
+    if($vm.StorageProfile.OsDisk.OsType -eq "Linux"){
+        [void]$vmoutput.Add([PSCustomObject]@{
+            "Machine Name" = $vm.Name
+            Nickname = $vm.tags.Role
+            "IP Address" = $nic.Properties.ipConfigurations.properties.privateIPAddress
+            "OS/ios/FW Version" = "$($vm.osname),$($vm.osversion)"
+            "Memory Size / Type" = "$($compute.memoryinmb/1024)GB / $($vm.hardwareprofile.vmsize)"
+        })
+    }
+    elseif(($vm.StorageProfile.OsDisk.OsType -eq "Windows")) {
+        [void]$vmoutput.Add([PSCustomObject]@{
+            "Machine Name" = $vm.Name
+            Nickname = $vm.tags.Role
+            "IP Address" = $nic.Properties.ipConfigurations.properties.privateIPAddress
+            "OS/ios/FW Version" = $vm.osname
+            "Memory Size / Type" = "$($compute.memoryinmb/1024)GB / $($vm.hardwareprofile.vmsize)"
+        })
+    } else {
+        write-output "$($vm.name) does not have a valid os type"
+    }
+}
